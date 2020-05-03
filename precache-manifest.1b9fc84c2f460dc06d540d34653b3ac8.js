91136a3d0cc9f47a1bb0f604d7e078aac9933c7f
Slava Borovsky <viachaslau.barouski@itechart-group.com>
2020-05-03 08:12:59 -0400

    Updates

---
+++ precache-manifest.1b9fc84c2f460dc06d540d34653b3ac8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437756dc8d547ea19cc9ffc59d299c88",
+    "url": "/covid-19-overview/index.html"
+  },
+  {
+    "revision": "d74ff8f01b9b71c67a9f",
+    "url": "/covid-19-overview/static/css/3.b76dadae.chunk.css"
+  },
+  {
+    "revision": "a903cee0a0edcbb6882e",
+    "url": "/covid-19-overview/static/css/main.e7a757be.chunk.css"
+  },
+  {
+    "revision": "13501533defeece3aa4c",
+    "url": "/covid-19-overview/static/js/2.988d7695.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/covid-19-overview/static/js/2.988d7695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74ff8f01b9b71c67a9f",
+    "url": "/covid-19-overview/static/js/3.cdd2d8e9.chunk.js"
+  },
+  {
+    "revision": "e98930b653c93523f79e",
+    "url": "/covid-19-overview/static/js/4.2a6e7673.chunk.js"
+  },
+  {
+    "revision": "a903cee0a0edcbb6882e",
+    "url": "/covid-19-overview/static/js/main.5f546a12.chunk.js"
+  },
+  {
+    "revision": "85b0edd3ad2376bf5b26",
+    "url": "/covid-19-overview/static/js/runtime-main.05816fc8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-overview/precache-manifest.16bec67ed5e0c6d4f3756eccb772485a.js"
+  "/covid-19-overview/precache-manifest.1b9fc84c2f460dc06d540d34653b3ac8.js"
 );
 
 self.addEventListener('message', (event) => {
