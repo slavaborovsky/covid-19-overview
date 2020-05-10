f0d627c1e0215472166ef1988a52594e17ececd8
Slava Borovsky <viachaslau.barouski@itechart-group.com>
2020-05-10 05:41:18 -0400

    use document data attrbiute to control theming

--- src/context/theme/context-provider.js
+++ src/context/theme/context-provider.js
@@ -1,28 +1,24 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ThemeSelectorContext, CURRENT_THEME } from './theme-context';
 
 export const ThemeContextProvider = ({ children }) => {
 	const [theme, setTheme] = useState(CURRENT_THEME);
-	const rootRef = useRef();
 
 	useEffect(() => {
-		rootRef.current = document.getElementById('root');
-		rootRef.current.classList = `covid-${CURRENT_THEME}-theme`;
+		if (CURRENT_THEME === 'dark') {
+			document.documentElement.setAttribute('data-theme', 'dark');
+		}
 		window.localStorage.setItem('app-theme', CURRENT_THEME);
-
-		return () => {
-			rootRef.current = undefined;
-		};
 	}, []);
 
 	const toggleTheme = () => {
 		if (theme === 'dark') {
-			rootRef.current.classList = 'covid-light-theme';
+			document.documentElement.removeAttribute('data-theme');
 			setTheme('light');
 			window.localStorage.setItem('app-theme', 'light');
 		} else {
-			rootRef.current.classList = 'covid-dark-theme';
+			document.documentElement.setAttribute('data-theme', 'dark');
 			setTheme('dark');
 			window.localStorage.setItem('app-theme', 'dark');
 		}
--- src/utils/get-css-property-value.js
+++ src/utils/get-css-property-value.js
@@ -1,13 +1,7 @@
-export const getCssPropertyValue = (keys, theme = 'light') => {
-	const themeElement = document.querySelector(`.covid-${theme}-theme`);
-	if (!themeElement) {
-		return {};
-	}
-
-	const computedStyles = window.getComputedStyle(themeElement);
+export const getCssPropertyValue = (keys) => {
+	const computedStyles = window.getComputedStyle(document.documentElement);
 	return keys.reduce((out, key) => {
 		const variable = `--covid-${key}`;
-
 		out[key] = computedStyles.getPropertyValue(variable);
 		return out;
 	}, {});
