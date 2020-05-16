e38184f18e9c765113ad0512624964ebb84d260f
Slava Borovsky <viachaslau.barouski@itechart-group.com>
2020-05-16 08:44:54 -0400

    Updates

---
+++ precache-manifest.6e4a2d2f7b5f58257f5c28511229680c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa20f4d3c71bfce12e9091f40bea83a7",
+    "url": "/covid-19-overview/index.html"
+  },
+  {
+    "revision": "925384edc0d0added883",
+    "url": "/covid-19-overview/static/css/4.e91423d6.chunk.css"
+  },
+  {
+    "revision": "5127c4d21c34e0e0f23e",
+    "url": "/covid-19-overview/static/css/main.bb8c2ade.chunk.css"
+  },
+  {
+    "revision": "9711eebc2a4c2e0d32d2",
+    "url": "/covid-19-overview/static/js/0.7babcae3.chunk.js"
+  },
+  {
+    "revision": "162347858619f237b9e3",
+    "url": "/covid-19-overview/static/js/3.69ae3722.chunk.js"
+  },
+  {
+    "revision": "af5111307236c4c69d6adba51f98588a",
+    "url": "/covid-19-overview/static/js/3.69ae3722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "925384edc0d0added883",
+    "url": "/covid-19-overview/static/js/4.fa53bd3a.chunk.js"
+  },
+  {
+    "revision": "8376256484c815120e62",
+    "url": "/covid-19-overview/static/js/5.33c0a51c.chunk.js"
+  },
+  {
+    "revision": "a56e9a1bf1540594f25c",
+    "url": "/covid-19-overview/static/js/6.0bbd69b8.chunk.js"
+  },
+  {
+    "revision": "e3ceaae4178a4e6b86c2",
+    "url": "/covid-19-overview/static/js/7.51c37d8f.chunk.js"
+  },
+  {
+    "revision": "5127c4d21c34e0e0f23e",
+    "url": "/covid-19-overview/static/js/main.76b058bb.chunk.js"
+  },
+  {
+    "revision": "13afd4a58d341435153e",
+    "url": "/covid-19-overview/static/js/runtime-main.c4f1a1c2.js"
+  },
+  {
+    "revision": "0e4f2fb405e8d90e230def2e4a7a8af8",
+    "url": "/covid-19-overview/static/media/world.0e4f2fb4.topojson"
+  }
+]);
