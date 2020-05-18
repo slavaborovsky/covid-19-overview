aff1f98348fb42bd44ee307aabb328c47be9bbfe
Slava Borovsky <viachaslau.barouski@itechart-group.com>
2020-05-18 15:25:33 -0400

    use stdin

--- scripts/deploy-heroku.sh
+++ scripts/deploy-heroku.sh
@@ -2,7 +2,7 @@
 echo "Running release $TAG"
 
 wget -qO- https://toolbelt.heroku.com/install.sh | sh && \
-docker login --username="$HEROKU_USERNAME" --password="$HEROKU_API_KEY" registry.heroku.com && \
+echo "$HEROKU_API_KEY" | docker  login --username="$HEROKU_USERNAME" --password-stdin registry.heroku.com && \
 docker tag "$IMAGE_STORAGE":"$TAG" registry.heroku.com/"$HEROKU_APP_NAME"/web && \
 docker push registry.heroku.com/"$HEROKU_APP_NAME"/web && \
 heroku container:release web --app "$HEROKU_APP_NAME"
